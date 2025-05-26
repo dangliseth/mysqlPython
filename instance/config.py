18e032c527f37e06bd8b35ffbc4d36ac22d3da3a
dangliseth <danglisr@students.nu-fairview.edu.ph>
2025-05-26 08:43:53 +0800

    ignore config

--- instance/config.py
+++ instance/config.py
@@ -4,5 +4,5 @@ import keyring
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-insecure-key')
 MYSQL_DATABASE_HOST = 'localhost'
 MYSQL_DATABASE_USER = 'root'
-MYSQL_DATABASE_PASSWORD = keyring.get_password("inventory_app_mysql", "root")
+MYSQL_DATABASE_PASSWORD = "w3Lcome2mLqu"
 MYSQL_DATABASE_DB = 'inventory_database'
